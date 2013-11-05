e4f909086007f37312b1dd25df31a08a601eba42
mikegrb <mgreb@linode.com>
2013-11-04 20:36:00 -0500

    HTML part for Taskwarrior plugin

--- lib/ExobrainPipeline/Plugin/Taskwarrior.pm
+++ lib/ExobrainPipeline/Plugin/Taskwarrior.pm
@@ -6,16 +6,22 @@ use 5.010;
 use Moose::Autobox;
 use namespace::autoclean;
 
-has cmd         => ( is => 'ro', isa => 'Str', required => 1 );
-has title       => ( is => 'ro', isa => 'Str', default  => 'Task Warrior' );
+has cmd   => ( is => 'ro', isa => 'Str', required => 1 );
+has title => ( is => 'ro', isa => 'Str', default  => 'Task Warrior' );
 
 sub execute {
     my ( $self, $data ) = @_;
 
-    my $output = $self->title . "\n" . '=' x length( $self->title ) . "\n";
-    $output .= `$self->{cmd}`;
+    my $output = `$self->{cmd}`;
 
-    $data->push( { agenda => $output } );
+    my $txt
+        = $self->title . "\n"
+        . '=' x length( $self->title ) . "\n"
+        . $output . "\n";
+
+    my $html = "<h1>" . $self->title . "</h1>\n<pre>$output</pre>\n";
+
+    $data->push( { agenda => { text => $txt, html => $html } } );
 
     return $data;
 }
