c428dce37b573e50670de2cb64a103c1a7b854d0
mikegrb <mgreb@linode.com>
2013-11-11 20:29:31 -0500

    Include TDP Streaks in email

--- lib/ExobrainPipeline/Plugin/TheDailyPractice.pm
+++ lib/ExobrainPipeline/Plugin/TheDailyPractice.pm
@@ -7,11 +7,13 @@ use 5.010;
 use Net::TDP;
 use DateTime;
 use Moose::Autobox;
+use List::Util qw(first);
 
 use namespace::autoclean;
 
 has api_key   => ( is => 'ro', isa => 'Str', required => 1 );
 has time_zone => ( is => 'ro', isa => 'Str', required => 0 );
+has title     => ( is => 'ro', isa => 'Str', default  => 'TDP Streaks' );
 has today => ( is => 'ro', isa => 'Bool', default => 0 ); # yesterday is default
 has _api => (
     is      => 'ro',
@@ -39,6 +41,24 @@ sub execute {
                 = $self->_api->completed( $goal->flatten, date => $date );
         }
     }
+
+    my $output    = '';
+    my $goal_data = [];
+    for my $goal (
+        $self->_api->list_goals->{goals}->grep( sub { $_->{active} } )
+        ->flatten )
+    {   my $today = first { $_->{date} eq $date } $goal->{trend}->flatten;
+        my $streak = $today->{streak};
+        $output .= "$goal->{name}: $streak\n";
+    }
+
+    my $txt
+        = $self->title . "\n"
+        . '=' x length( $self->title ) . "\n"
+        . $output . "\n";
+
+    my $html = "<h1>" . $self->title . "</h1>\n<pre>$output</pre>\n";
+    $data->push( { agenda => { text => $txt, html => $html } } );
 
     return $data;
 }
